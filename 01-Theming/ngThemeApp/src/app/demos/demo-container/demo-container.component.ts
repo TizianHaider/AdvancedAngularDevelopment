4f0dfe269d2b1d2c389bad41479d8776b7ebfc27
TizianHaider <tizian.haider@gmx.at>
2020-08-10 13:38:42 +0200

    tslint fix

--- 01-Theming/ngThemeApp/src/app/demos/demo-container/demo-container.component.ts
+++ 01-Theming/ngThemeApp/src/app/demos/demo-container/demo-container.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { Event, NavigationEnd, Router } from "@angular/router";
-import { filter, flatMap, map } from "rxjs/operators";
-import { Observable } from "rxjs";
-import { MenuService } from "src/app/shared/menu/menu.service";
-import { DemoItem } from "../demo-item";
-import { DemoService } from "../demo.service";
-import { environment } from "src/environments/environment";
-import { EventBusService } from "src/app/shared/eventbus/event-bus.service";
-import { SidebarActions } from "src/app/shared/sidebar/sidebar-actions";
+import { Component, OnInit } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { filter, flatMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { MenuService } from 'src/app/shared/menu/menu.service';
+import { DemoItem } from '../demo-item';
+import { DemoService } from '../demo.service';
+import { environment } from 'src/environments/environment';
+import { EventBusService } from 'src/app/shared/eventbus/event-bus.service';
+import { SidebarActions } from 'src/app/shared/sidebar/sidebar-actions';
 
 @Component({
-  selector: "app-demo-container",
-  templateUrl: "./demo-container.component.html",
-  styleUrls: ["./demo-container.component.scss"]
+  selector: 'app-demo-container',
+  templateUrl: './demo-container.component.html',
+  styleUrls: ['./demo-container.component.scss'],
 })
 export class DemoContainerComponent implements OnInit {
   title: string = environment.title;
-  header: string = "Please select a demo";
+  header = 'Please select a demo';
 
   demos$: Observable<DemoItem[]> = null;
   current: DemoItem = this.demos$ != null ? this.demos$[0] : null;
   workbench: any;
 
-  showEditor: boolean = false;
+  showEditor = false;
 
   constructor(
     public ms: MenuService,
@@ -43,17 +43,17 @@ export class DemoContainerComponent implements OnInit {
   }
 
   setEditor() {
-    this.eb.Commands.subscribe(action => {
+    this.eb.Commands.subscribe((action) => {
       this.showEditor = action == SidebarActions.HIDE_MARKDOWN ? false : true;
     });
   }
 
   getWorbenchStyle() {
     let result = {};
-    this.ms.visible$.subscribe(visible => {
+    this.ms.visible$.subscribe((visible) => {
       result = visible
         ? {
-            "margin-left": "10px"
+            'margin-left': '10px',
           }
         : {};
     });
@@ -65,17 +65,17 @@ export class DemoContainerComponent implements OnInit {
       .pipe(
         filter((evt: Event) => evt instanceof NavigationEnd),
         flatMap((evt: NavigationEnd) => {
-          let childroute = evt.url.substr(evt.url.lastIndexOf("/") + 1);
+          const childroute = evt.url.substr(evt.url.lastIndexOf('/') + 1);
           return this.demos$.pipe(
-            map(items => items.find(i => i.url.includes(childroute)))
+            map((items) => items.find((i) => i.url.includes(childroute)))
           );
         })
       )
-      .subscribe(demo => {
+      .subscribe((demo) => {
         this.header =
           demo != null
             ? `Demo: ${demo.title} - Component: ${demo.component}`
-            : "Please select a demo";
+            : 'Please select a demo';
       });
   }
 }
