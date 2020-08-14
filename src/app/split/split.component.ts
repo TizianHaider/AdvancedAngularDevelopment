e9a684fe563395e1d1d721e0bcb0bdfd52abb5aa
TizianHaider <tizian.haider@gmx.at>
2020-08-14 10:10:16 +0200

    inital commit lib lab;

---
+++ src/app/split/split.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ux-split',
+  templateUrl: './split.component.html',
+  styleUrls: ['./split.component.scss'],
+})
+export class SplitComponent implements OnInit {
+  constructor() {}
+
+  toolbar = '100px';
+
+  ngOnInit() {}
+}
